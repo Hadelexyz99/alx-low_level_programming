ce2b12d15a3e1d65c4c034ea66cc000c3af232b2
Adeleye Usman <usmanleye@gmail.com>
2022-09-08 06:02:20 -0700

    God Abeg4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+/**
+ *
+ *    * main - print to screen
+ *
+ *       *
+ *
+ *          * Return: zero
+ *
+ *             */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return 0;
+	return (0);
 }
