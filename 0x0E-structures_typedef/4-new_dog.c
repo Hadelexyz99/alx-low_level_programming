213a1987b1580acadaf0e830adafe7aef31929b8
Adeleye Usman <usmanleye@gmail.com>
2022-10-10 23:43:11 -0700

    ok

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -62,4 +62,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
 
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+
+	return (doggo);
+}
